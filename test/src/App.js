238d4fb3806375227c17149fbb2d0fe9c48e54c0
Omer <omerkaraman@hotmail.co.uk>
2021-03-10 21:02:29 +0000

    made changes

--- test/src/App.js
+++ test/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from "@material-ui/core";
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button variant="contained" color="primary" onClick={() => setCount(count + 1)}>
+      Clicked {count} times!
+    </Button>
   );
 }
 
